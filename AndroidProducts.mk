af22733a9986002c95e2e61ffd50291c358dd016
Dan Willemsen <dwillemsen@google.com>
2020-06-15 19:38:53 +0800

    j5xltecmcc: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: I80bace95169714c32d5a0d3f810d5d3ad3acace8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,3 +3,8 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/omni_j5xltecmcc.mk \
 	$(LOCAL_DIR)/rr_j5xltecmcc.mk \
 	$(LOCAL_DIR)/lineage_j5xltecmcc.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_j5xltecmcc-eng \
+    lineage_j5xltecmcc-userdebug \
+    lineage_j5xltecmcc-user
