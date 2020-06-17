f27bfd641a8c895182c3eb48ee60acf5fd312d45
zqm64 <zqm64@outlook.com>
2020-06-18 03:38:29 +0800

    j5xltecmcc: Add Inherit from NXP PN547 NFC Support
    Change-Id: I7f7f6dac4be6cd12f4e7c6130cc8ee388bf5813a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ TARGET_OTA_ASSERT_DEVICE := j5xltecmcc,j5xltezm
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_j5xlte_chncmcc_defconfig
 
+# NFC
+-include device/samsung/msm8916-common/nfc/pn547/board.mk
+
 # Partition sizes
 BOARD_SYSTEMIMAGE_PARTITION_SIZE    := 2835349504
 BOARD_USERDATAIMAGE_PARTITION_SIZE  := 12511588352
